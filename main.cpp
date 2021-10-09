57fc0bca696ae2ca39c4fbad6e97a013d033f112
Mouadzz <mlasrite@student.1337.ma>
2021-10-09 17:04:16 +0100

    insert done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlasrite <mlasrite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 11:07:30 by mlasrite          #+#    #+#             */
-/*   Updated: 2021/10/09 13:49:55 by mlasrite         ###   ########.fr       */
+/*   Updated: 2021/10/09 17:02:57 by mlasrite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -148,33 +148,25 @@ int main()
   // // iterator_tests();
 
   // ---------------------------------------------------- //
-  ft::vector<int> myvector(6, 2);
-  ft::vector<int>::iterator it;
+  std::vector<int> myvector(3, 100);
+  std::vector<int>::iterator it;
 
-  myvector.reserve(10);
+  it = myvector.begin();
+  it = myvector.insert(it, 200);
 
-  it = myvector.end() - 3;
-  it = myvector.insert(it, 1);
+  myvector.insert(it, 2, 300);
 
+  // "it" no longer valid, get a new one:
+  it = myvector.begin();
+
+  std::vector<int> anothervector(2, 400);
+  myvector.insert(it + 2, anothervector.begin(), anothervector.end());
+
+  int myarray[] = {501, 502, 503};
+  myvector.insert(myvector.begin(), myarray, myarray + 3);
+
+  std::cout << "myvector contains:";
   for (it = myvector.begin(); it < myvector.end(); it++)
     std::cout << ' ' << *it;
   std::cout << '\n';
-
-  
-
-  // myvector.insert(it, 2, 300);
-
-  // // "it" no longer valid, get a new one:
-  // it = myvector.begin();
-
-  // std::vector<int> anothervector(2, 400);
-  // myvector.insert(it + 2, anothervector.begin(), anothervector.end());
-
-  // int myarray[] = {501, 502, 503};
-  // myvector.insert(myvector.begin(), myarray, myarray + 3);
-
-  // std::cout << "myvector contains:";
-  // for (it = myvector.begin(); it < myvector.end(); it++)
-  //   std::cout << ' ' << *it;
-  // std::cout << '\n';
 }
